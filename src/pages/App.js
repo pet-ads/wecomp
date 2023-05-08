9604a37cdaa3766692c63fafb6142723eef46e9c
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-05-08 13:33:17 -0300

    fix: alteração das propriedades width e max-width para evitar uma barra de rolagem horizontal.

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,15 +1,16 @@
 import '../style/pages/App.css';
 import Header from '../components/Header.js';
 import Footer from '../components/Footer.js';
+import Banner from '../components/Banner.js';
 
 function App() {
   return (
     <div className="app">
       <Header/>
 
-      <div className="banner">
-
-      </div>
+      <Banner bannerImage="https://via.placeholder.com/1000x500">
+        <h1>Wecomp</h1>
+      </Banner>
 
       <main className="content">
 
