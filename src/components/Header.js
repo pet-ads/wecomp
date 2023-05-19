848bd36c134842561a19faca233926c9335d4717
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-05-19 14:58:24 -0300

    ajuste nos arquivos CSS e pequenas mudanças nos componentes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,6 @@ import Navbar from './Navbar';
 function Header(){
     return (
         <header className="header">
-            <img className="logo" src="https://via.placeholder.com/48x48" alt="Logo da Wecomp"/>
             <Navbar/>
         </header>
     )
