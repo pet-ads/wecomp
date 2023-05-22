bd1f06e0f32e7ddff24d1974eb7ded29bc344c9a
luiz-chiq <chiquetanoluiz@gmail.com>
2023-05-22 00:13:44 -0300

    mapa(beta) e realizadores adicionados

--- src/pages/Supporters.js
+++ src/pages/Supporters.js
@@ -1,14 +1,19 @@
-import Patrocinador from "../components/Supporter";
+import Supporter from "../components/Supporter";
 import "../style/pages/Supporters.css";
 
 const logos = [
-    ["http://via.placeholder.com/640x360", "Lego"],
-    ["http://via.placeholder.com/100x50", "Lego"],
-    ["http://via.placeholder.com/300x300", "Lego"],
-    ["http://via.placeholder.com/400x200", "Lego"],
-    ["http://via.placeholder.com/200x400", "Lego"],
-    
+    ["http://via.placeholder.com/640x360", "Patrocinador1"],
+    ["http://via.placeholder.com/100x50", "Patrocinador2"],
+    ["http://via.placeholder.com/300x300", "Patrocinador3"],
+    ["http://via.placeholder.com/400x200", "Patrocinador4"],
+    ["http://via.placeholder.com/200x400", "Patrocinador5"],
     ]
+
+const logos2 = [
+    ["https://iamfounder.com.br/images/logo-onovolab.png", "Onovolab"],
+    ["http://pet_ads.paginas.scl.ifsp.edu.br/assets/logo_PET.svg", "PET/ADS"],
+    ["https://portais.ifsp.edu.br/scl/images/Logo_Campus/Marca_IFSP_2015_Sao-Carlos.png", "IFSP - São Carlos"],
+]
 
 function Supporters(){
     return(
@@ -18,7 +23,15 @@ function Supporters(){
 
            
             {logos.map((patrocinador, index) => (
-                <Patrocinador key={index} patrocinador={patrocinador} />
+                <Supporter key={index} patrocinador={patrocinador} />
+                ))}
+            </div>
+            <h1 className="texto">Realização</h1>
+            <div className="patrocinadores">
+
+           
+            {logos2.map((patrocinador, index) => (
+                <Supporter key={index} patrocinador={patrocinador} />
                 ))}
             </div>
         </div>
