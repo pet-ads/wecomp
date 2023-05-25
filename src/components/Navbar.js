a6ec60f7ef7b03c62b8110a9f14c5b408b6cb7ee
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-05-25 15:05:51 -0300

    consertando os links da navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,12 @@ function Navbar(){
     return (
         <nav className="nav-bar">
             <ul>
-                <li><a href="#banner">Início</a></li>
-                <li><a href="#about-the-event">Sobre</a></li>
-                <li><a href="#patrocinios">Apoio</a></li>
-                <li><a href="#patrocinios">Cronograma</a></li>
-                <li><a href="#patrocinios">Realização</a></li>
-                <li><a href="#local">Local</a></li>
+                <li><a className="navBanner" href="#">Início</a></li>
+                <li><a className="navAbout" href="#about-the-event">Sobre</a></li>
+                <li><a className="navSupports" href="#organizers">Apoio</a></li>
+                <li><a className="navSchedule" href="#">Cronograma</a></li>
+                <li><a className="navOrganizers" href="#organizers">Realização</a></li>
+                <li><a className="navLocation" href="#local">Local</a></li>
             </ul>
         </nav>
     )
