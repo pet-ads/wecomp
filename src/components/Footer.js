7d37366e387cd41718b41d6dfc68bbf84964bb22
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-05-30 01:02:34 -0300

    adição do github do projeto no footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,16 @@
 import '../style/components/Footer.css';
 import logoIFSPbranco from '../images/logoIFSPbranco.png';
+import logoGithubBranco from '../images/github-mark-white.png';
 
 function Footer(){
     return (
         <footer id="footer" className="footer">
+            <a href="https://github.com/pet-ads/wecomp" target="_blank">
+                <img className="logoGithub" src={logoGithubBranco} alt="Logo do Github"/>
+            </a>
             <p>Desenvolvido com ♥ pela equipe do PET 
                 do curso de Análise e Desenvolvimento de Sistemas</p>
-            <img className="logoIF" src={logoIFSPbranco} alt="Logo do IFSP"/>
+            <img className="logoIF" src={logoIFSPbranco} alt="Logo do IFSP"/>  
         </footer>
     )
 }
