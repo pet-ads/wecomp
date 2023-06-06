d8114daf131d380f732d345bd8c325cea1f61e2b
luiz-chiq <chiquetanoluiz@gmail.com>
2023-06-05 23:24:07 -0300

    feat: apoiadores adicionados

--- src/components/GroupOfPartners.js
+++ src/components/GroupOfPartners.js
@@ -5,7 +5,9 @@ function GroupOfPartners({partners, text}){
 
     return (
         <>
-            <h1 className="partnersTitle">{text}</h1>
+            <div className="partnersTitleContainer">
+                <h1 className="partnersTitle">{text}</h1>
+            </div>
             <div className="groupOfPartners">
                 {partners.map((partner) => (
                     <Partner key={partner.id} partner={partner} image={partner.src}/>
