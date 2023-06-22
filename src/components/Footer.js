ec86192f65d71c0f379d5e034a3de9f428059eca
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-06-22 01:38:32 -0300

    mudando o footer para funcionar em telas menores

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,14 @@
 import '../style/components/Footer.css';
-import logoIFSPbranco from '../images/logoIFSPbranco.png';
 import logoGithubBranco from '../images/github-mark-white.png';
 
 function Footer(){
     return (
         <footer id="footer" className="footer">
-            <a href="https://github.com/pet-ads/wecomp" target="_blank">
+            <a href="https://github.com/pet-ads/wecomp">
                 <img className="logoGithub" src={logoGithubBranco} alt="Logo do Github"/>
             </a>
             <p>Desenvolvido com ♥ pela equipe do PET 
                 do curso de Análise e Desenvolvimento de Sistemas</p>
-            <img className="logoIF" src={logoIFSPbranco} alt="Logo do IFSP"/>  
         </footer>
     )
 }
