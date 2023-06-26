7058162be58a8b3a3a5ec892f0738f65ed3d88c2
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-06-25 22:58:07 -0300

    mudando o background

--- src/components/Background.js
+++ src/components/Background.js
@@ -4,11 +4,13 @@ function Background(){
     return (
         <>
             <div className="parallax_layer_back parallax__layer">
-                {/* <img src=""/> */}
+                <img className="parallaxImage1" alt="parallax" src="../logos/Subtract3.png"/>
+                <img className="parallaxImage2" alt="parallax" src="../logos/Subtract3.png"/>
+                
             </div>
-            <div className="parallax_layer_back_image_test1 parallax__layer"></div>
+           { /*<div className="parallax_layer_back_image_test1 parallax__layer"></div>
             <div className="parallax_layer_back_image_test2 parallax__layer"></div>
-            {/* <div className="parallax_layer_back_image_test3 parallax__layer"></div> */}
+            <div className="parallax_layer_back_image_test3 parallax__layer"></div> */}
         </>
     )
 }
