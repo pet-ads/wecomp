938b911159ffa6be77e1ffeb81d438a12a5a5567
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-07-06 21:21:57 -0300

    fix: 2snd attempt to fix broken footer in small sreens

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,18 +5,15 @@ import logoIfspBranco from '../images/logoIFSPbranco.png';
 function Footer(){
     return (
         <footer id="footer" className="footer">
-            <div>
-
-                <a href="https://github.com/pet-ads/wecomp">
-                    <img className="logoGithub" src={logoGithubBranco} alt="Logo do Github"/>
-                </a>
-                <a>
-                    <p className="footer_text">Desenvolvido com ♥ por PET/ADS @ IFSP/SCL</p>
-                </a>
-                <a href="https://portais.ifsp.edu.br/scl/">
-                    <img className='logoIfsp ' src={logoIfspBranco} alt="logo do IFSP"></img>
-                </a>
-            </div>
+            <a href="https://github.com/pet-ads/wecomp">
+                <img className="logoGithub" src={logoGithubBranco} alt="Logo do Github"/>
+            </a>
+            <a>
+                <p className="footer_text">Desenvolvido com ♥ por PET/ADS @ IFSP/SCL</p>
+            </a>
+            <a href="https://portais.ifsp.edu.br/scl/">
+                <img className='logoIfsp ' src={logoIfspBranco} alt="logo do IFSP"></img>
+            </a>
         </footer>
     )
 }
