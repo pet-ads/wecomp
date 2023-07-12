c650f7e5278361057ccadcf9c4e09fff8711a4f2
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-07-12 11:06:32 -0300

    feat: improved carousel control buttons/icons and Event component responsivity

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,5 +1,3 @@
-import Button from "./Button.js"
-import Event from "../components/Event"
 import "../style/components/Carousel.css"
 import "../style/components/Button.css"
 import { useState, useEffect } from "react"
@@ -68,6 +66,7 @@ function Carousel({ children }){
 
     const handleTouchStart = (event) => {
         setTouchPosition(event.touches[0].clientX)
+        console.log("touch start")
     }
 
     const handleTouchMove = (event) => {
@@ -117,7 +116,9 @@ function Carousel({ children }){
     return (
         <div className="carousel-control">
             <div className="carousel-window">
-                <div className="carousel" style={{ ...carouselTranformation, ...windowJustification }} >{
+                <div className="carousel" style={{ ...carouselTranformation, ...windowJustification }} 
+                        onTouchStart={ handleTouchStart } onTouchMove={ handleTouchMove } 
+                        onTouchEnd={ handleTouchEnd } >{
                     children.map(child => (
                         <div key={++countChildren} className="carousel-card">
                             { child }
@@ -128,12 +129,20 @@ function Carousel({ children }){
 
             { isAllowedToMoveBackward() &&
                 <div className="control-button previous">
-                    <input type="button" name="previous" value="<" onClick={ moveBackward } />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" 
+                            fill="currentColor" class="bi bi-chevron-compact-left" viewBox="0 0 16 16"
+                            onClick={ moveBackward }>
+                        <path fill-rule="evenodd" d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223z"/>
+                    </svg>
                 </div>
             }
             { isAllowedToMoveForward() &&
                 <div className="control-button next">
-                    <input type="button" className="next" value=">" onClick={ moveForward } />
+                    <svg class="next" xmlns="http://www.w3.org/2000/svg" width="16" height="16" 
+                            fill="currentColor" className="bi bi-chevron-compact-right" viewBox="0 0 16 16" 
+                            onClick={ moveForward }>
+                        <path d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"/>
+                    </svg>
                 </div>
             }
         </div>
