6b6facdfca7a0c8128b672a66f4d1e932f5ae613
DayaneVarella <dayaneptv@gmail.com>
2023-07-12 21:07:00 -0300

    Updating layout again

--- src/components/Background.js
+++ src/components/Background.js
@@ -4,7 +4,7 @@ import '../style/components/Parallax.css';
 function Background(){
     return (
         <div className="parallax_layer_back parallax__layer">
-            <img className="parallax_image" alt="parallax" src="../logos/BackgroundPattern.png"/>
+            <div className="parallax-image"/>
         </div>
     )
 }
