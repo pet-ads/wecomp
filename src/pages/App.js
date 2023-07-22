dc2e8d7856d9f915ba286d3c63992e8cfbba12e3
luiz-chiq <chiquetanoluiz@gmail.com>
2023-07-21 22:49:54 -0300

    fix

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,23 +1,24 @@
 import '../style/pages/App.css';
 import '../style/components/Parallax.css';
-import { isIOS, isMacOs } from "react-device-detect";
+import { isIOS, isMacOs, useMobileOrientation, isMobile } from "react-device-detect";
 
 import NormalLayout from "../components/NormalLayout";
 import SafariLayout from "../components/SafariLayout";
 
 
 function App() {
+  const orientation = useMobileOrientation();
   if (isIOS || isMacOs) {
     return (
       <div className="app">
-        <SafariLayout/>
+        <SafariLayout isLandscape={orientation.isLandscape && isMobile}/>
       </div>
     )
   }
 
   return (
     <div className="app">
-      <NormalLayout/>
+      <NormalLayout isLandscape={orientation.isLandscape && isMobile}/>
     </div>
   );
 }
