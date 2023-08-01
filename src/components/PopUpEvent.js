882a8d7784670007c10b95d65e95cd4e282626ce
luiz-chiq <chiquetanoluiz@gmail.com>
2023-08-01 03:54:42 -0300

    fix

--- src/components/PopUpEvent.js
+++ src/components/PopUpEvent.js
@@ -14,17 +14,16 @@ function PopUpEvent({name, author, date, description, classification}) {
   return (
     <div className='popUp'>
         <Button text='Descrição' action={() => setIsOpen(true)}/>
-
         <ReactModal className="popUpTeste" overlayClassName="popUpOverlay"
             isOpen={isOpen}
             contentLabel="Example Modal">
             <div className='popUpDescription'>
+                <h2>{name}</h2>
+                <h3>{author}</h3>
                 <div className="badge-line">
                   <ClassificationBadge classification={classification}/>
-                  <h2>{name}</h2>
+                  <p>{date}</p>
                 </div>
-                <h3>{author}</h3>
-                <p>{date}</p>
                 <p>{description}</p>
                 <Button text='Fechar' action={() => setIsOpen(false)}/>
             </div>
