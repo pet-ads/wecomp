d9fb1afd87ee58dc8885c19c8b2e566fdcfe636d
luiz-chiq <chiquetanoluiz@gmail.com>
2023-08-02 04:04:41 -0300

    fix: carousel responsiveness

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -48,9 +48,11 @@ function Carousel({ children }){
     }
 
     const moveForward = () => {
+        console.log("index: " + index)
+        console.log(maxNumberOfVisibleCards)
+        console.log(getCurrentCardWidth());
         if (!isAllowedToMoveForward())
             return
-        
         setCarouselPosition(calculateFixedPosition(index + 1))
         setIndex(index + 1)
     }
