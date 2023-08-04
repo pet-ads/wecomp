0cb84dbe7793cf18418a3fe6d86cd4fb11b9f511
luiz-chiq <chiquetanoluiz@gmail.com>
2023-08-04 20:12:23 -0300

    little fix

--- src/pages/AboutTheEvent.js
+++ src/pages/AboutTheEvent.js
@@ -8,7 +8,7 @@ function AboutTheEvent() {
                 <h2>Sobre a Wecomp</h2>
             </header>
             <div className="section-content">
-                <p>
+                <p className='event-description'>
                     A Wecomp é a semana da computação do IFSP São Carlos, que neste ano realizará sua quarta edição. O objetivo do evento é proporcionar uma maior interação entre os estudantes, o mercado de tecnologia e a cidade de São Carlos. Ao longo de cinco dias de evento, serão realizados diversos minicursos, talks e processos seletivos, que visam reduzir a distância entre a teoria e a prática, ensinar tecnologias novas e promover o ingresso dos participantes no mercado de trabalho
                 </p>
             </div>
