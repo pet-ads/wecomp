f8bd4889205bd12b169d1e706afa8c8e6baba7e7
luiz-chiq <chiquetanoluiz@gmail.com>
2023-08-14 14:33:58 -0300

    feat: disable button option

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,16 @@
 import '../style/components/Button.css';
 
-function Button({text, address, action, alt}){
+function Button({text, address, action, alt, active}){
     return (
-        <div className="button">
-            <a href={address}>
-                <input type="button" value={text} onClick={action}/>
-            </a>
+        <div>
+            {active == "false" ?
+                    <input type="button" value={text} className="disable"/>:
+                <a href={address}>
+                    <input type="button" value={text} className="button" onClick={action}/> 
+                </a>
+                    }
         </div>
     );
 }
 
-export default Button;
+export default Button
