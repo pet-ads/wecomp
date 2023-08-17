7e769ed71ef8df30dfa9b2e300eea3a8ddb98312
luiz-chiq <chiquetanoluiz@gmail.com>
2023-08-17 17:06:38 -0300

    feat: enrollment period on banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,9 +5,10 @@ function Banner({ bannerImage, canvasBackgroundColor, children }) {
     return (
         <div id="banner" className="banner">
             <div className="banner-container">
-                <img className="banner--image" src={logoWecomp}></img>
+                <img className="banner-image" src={logoWecomp}></img>
                 <h2 className="banner-title">IV Semana da Computação</h2>
                 <p className="banner-text">28 de agosto a 01 de setembro</p>
+                <p className="enrollment-period">Inscrições de 18 a 22 de agosto</p>
             </div>
         </div> 
     );
