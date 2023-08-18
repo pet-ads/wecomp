99e4cffb145ac24fed031276bd8ca5328a6bf52e
luiz-chiq <chiquetanoluiz@gmail.com>
2023-08-17 23:59:08 -0300

    feat: open registration

--- src/components/PopUpEvent.js
+++ src/components/PopUpEvent.js
@@ -11,7 +11,7 @@ import PopUpEventTab from './PopUpEventTab.js';
 function PopUpEvent({event}) {
   const [isOpen, setIsOpen] = useState(false);
  
-  const registrationPeriodActive = false;
+  const registrationPeriodActive = true;
 
   return (
     <div className='popUp'>
