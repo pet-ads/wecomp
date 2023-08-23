e01d414859b5a7cdae9dfde476faad1e8252bc55
luiz-chiq <116838721+luiz-chiq@users.noreply.github.com>
2023-08-23 15:31:46 -0300

    feat: remove enrollment period text on Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,7 +8,6 @@ function Banner({ bannerImage, canvasBackgroundColor, children }) {
                 <img className="banner-image" src={logoWecomp}></img>
                 <h2 className="banner-title">IV Semana da Computação</h2>
                 <p className="banner-text">28 de agosto a 01 de setembro</p>
-                <p className="enrollment-period">Inscrições de 18 a 22 de agosto</p>
             </div>
         </div> 
     );
