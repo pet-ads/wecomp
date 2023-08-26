f7f9b395b57920c4befa7b1dd7d7570db813d6e8
luiz-chiq <chiquetanoluiz@gmail.com>
2023-08-26 20:04:56 -0300

    feat: button open new tabs instead of redirect to new page

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ function Button({text, address, action, alt, active}){
         <div>
             {active == false ?
                     <input type="button" value={text} className="disable"/>:
-                <a href={address}>
+                <a href={address} target="_blank">
                     <input type="button" value={text} className="button" onClick={action}/> 
                 </a>
                     }
